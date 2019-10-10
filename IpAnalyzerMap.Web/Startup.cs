7e249898a78320707b7e505b20369e67251bffe9
egolovin1996 <egolovin1996@gmail.com>
2019-10-10 15:44:05 +0500

    #2 Add ErrorHandling

--- IpAnalyzerMap.Web/Startup.cs
+++ IpAnalyzerMap.Web/Startup.cs
@@ -1,3 +1,4 @@
+using IpAnalyzerMap.Web.Middleware;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -16,17 +17,14 @@ namespace IpAnalyzerMap.Web
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-
-            // In production, the React files will be served from this directory
+            
             services.AddSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp/build"; });
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,7 +34,6 @@ namespace IpAnalyzerMap.Web
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
@@ -62,6 +59,8 @@ namespace IpAnalyzerMap.Web
                     spa.UseReactDevelopmentServer(npmScript: "start");
                 }
             });
+
+            app.UseMiddleware<ErrorHandlingMiddleware>();
         }
     }
 }
